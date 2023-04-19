be1dc350445a23f292e445f3ce458536c64e591a
vishal sawai <vishalsawai1725@gmail.com>
2023-04-19 18:58:55 +0530

    favicon added

--- admin/dashboard/message.php
+++ admin/dashboard/message.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 
     <title>Messsage</title>
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OPENCODE</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
     <!-- css -->
@@ -12,6 +13,7 @@
 
     <!-- font aweosome -->
     <script src="https://kit.fontawesome.com/ff3c91d27a.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
