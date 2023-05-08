f941b52c9b32ab6e66cdd8fc8059b8e91c767cea
vishal sawai <vishalsawai1725@gmail.com>
2023-05-08 21:40:12 +0530

    j

--- index.php
+++ index.php
@@ -811,7 +811,7 @@
         <!-- Grid container -->
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
+            © 2023 Copyright:
             <a class="text-white" href="index.php">OPENCODE</a>
             All Rights Reserved
         </div>
